b56c57814642fd6236cc3e5d4531db7e97d79555
Eugene <terrestrus@gmail.com>
2017-07-05 22:21:09 +0300

    Статистика показывает правильный результат

--- js/model/WinResult.js
+++ js/model/WinResult.js
@@ -1,8 +1,8 @@
 import renderElement from '../lib/Render';
 import WinResultView from '../view/WinResultView';
 import App from '../Main';
-import {sortStat} from '../lib/Utils';
 
+export let latestResult;
 
 class WinResult {
   constructor(state, model) {
@@ -11,37 +11,16 @@ class WinResult {
     this.view = new WinResultView(this.state);
   }
 
-  findPercent(latest) {
-
-    this.model.getStat()
-      .then((data) => {
-        const sortedStatistics = sortStat(data);
-
-        sortedStatistics.forEach((result, index) => {
-          if (result.answers === latest.answers &&
-            result.time === latest.time) {
-            this.state = sortedStatistics[index];
-            if ((index + 1) / sortedStatistics.length === 1) {
-              this.state.percent = 0;
-            } else {
-              this.state.percent = Math.round(100 - ((index + 1) / sortedStatistics.length * 100));
-
-            }
-          }
-        });
-      });
-  }
   init() {
     if (!this.state.playerAnswers) {
       this.view = new WinResultView(this.state);
       renderElement(this.view);
     } else {
-      this.state.latestResult = {
+      latestResult = {
         time: this.state.totalTime,
         answers: this.state.playerAnswers,
       };
       App.showStats(this.state.scoresForAnswer);
-      this.findPercent(this.state.latestResult);
 
     }
 
